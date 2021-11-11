15988ac9333400e43b7a0d1df96a51692e73d8af
yosadade <masyosad@gmail.com>
2021-11-11 20:06:21 +0700

    integrasi api dashboard finance

--- src/components/moleculs/CardDeveloper/index.js
+++ src/components/moleculs/CardDeveloper/index.js
@@ -1,69 +1,83 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
-import {Gap} from '../..';
-import {ICArrowUp, ICEdit, ICTrash} from '../../../assets';
+import {ICArrowUp} from '../../../assets';
 import {fonts} from '../../../utils';
+import {useDispatch, useSelector} from 'react-redux';
 
 const CardDeveloper = () => {
+  const [viewDev, setViewDev] = useState(null);
+  const [projectly, setProjectly] = useState(0);
+  const [devSharing, setDevSharing] = useState(0);
+  const [taskly, setTaskly] = useState(0);
+  const [startUp, setStartUp] = useState(0);
+
+  const {project} = useSelector(state => state.projectReducer);
+
+  const dataDeveloper = [
+    {
+      id: 1,
+      keterangan: 'Projectly',
+      jml: projectly.length,
+      desc: projectly,
+    },
+    {
+      id: 2,
+      keterangan: 'Dev Sharing',
+      jml: devSharing.length,
+      desc: devSharing,
+    },
+    {
+      id: 3,
+      keterangan: 'Taskly',
+      jml: taskly.length,
+      desc: taskly,
+    },
+    {
+      id: 4,
+      keterangan: 'Start Up',
+      jml: startUp.length,
+      desc: startUp,
+    },
+  ];
+
+  useEffect(() => {
+    const filteredProjectly = project.filter(
+      data => data.model === 'Projectly',
+    );
+    const filterDevSharing = project.filter(
+      data => data.model === 'Dev Sharing',
+    );
+    const filteredTaskly = project.filter(data => data.model === 'Taskly');
+    const filteredStartUp = project.filter(data => data.model === 'Start Up');
+
+    setProjectly(filteredProjectly);
+    setDevSharing(filterDevSharing);
+    setTaskly(filteredTaskly);
+    setStartUp(filteredStartUp);
+  }, [project]);
+
   return (
     <View style={styles.container}>
       <Text style={styles.label}>Developer</Text>
-      <View style={styles.wrapper}>
-        <View style={styles.wrapperCash}>
-          <View style={styles.image} />
-          <View>
-            <Text style={styles.title}>Projectly</Text>
-            <Text style={styles.subTitle}>9</Text>
+      {dataDeveloper.map(item => {
+        const {id, keterangan, jml} = item;
+        return (
+          <View style={styles.wrapper} key={id}>
+            <View style={styles.wrapperCash}>
+              <View style={styles.image} />
+              <View>
+                <Text style={styles.title}>{keterangan}</Text>
+                <Text style={styles.subTitle}>{jml}</Text>
+              </View>
+            </View>
+            <View style={styles.wrapperBtn}>
+              <TouchableOpacity style={styles.btnArrow}>
+                <ICArrowUp />
+              </TouchableOpacity>
+            </View>
           </View>
-        </View>
-        <View style={styles.wrapperBtn}>
-          <TouchableOpacity style={styles.btnArrow}>
-            <ICArrowUp />
-          </TouchableOpacity>
-        </View>
-      </View>
-      <View style={styles.wrapper}>
-        <View style={styles.wrapperCash}>
-          <View style={styles.image} />
-          <View>
-            <Text style={styles.title}>Dev Sharing</Text>
-            <Text style={styles.subTitle}>5</Text>
-          </View>
-        </View>
-        <View style={styles.wrapperBtn}>
-          <TouchableOpacity style={styles.btnArrow}>
-            <ICArrowUp />
-          </TouchableOpacity>
-        </View>
-      </View>
-      <View style={styles.wrapper}>
-        <View style={styles.wrapperCash}>
-          <View style={styles.image} />
-          <View>
-            <Text style={styles.title}>Taskly</Text>
-            <Text style={styles.subTitle}>5</Text>
-          </View>
-        </View>
-        <View style={styles.wrapperBtn}>
-          <TouchableOpacity style={styles.btnArrow}>
-            <ICArrowUp />
-          </TouchableOpacity>
-        </View>
-      </View>
-      <View style={styles.wrapper}>
-        <View style={styles.wrapperCash}>
-          <View style={styles.image} />
-          <View>
-            <Text style={styles.title}>Start Up</Text>
-            <Text style={styles.subTitle}>3</Text>
-          </View>
-        </View>
-        <View style={styles.wrapperBtn}>
-          <TouchableOpacity style={styles.btnArrow}>
-            <ICArrowUp />
-          </TouchableOpacity>
-        </View>
-      </View>
+        );
+      })}
     </View>
   );
 };
